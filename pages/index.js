904469d04a803b2209e4b313c1c78f899caecaca
Ajay Phogat <ajayphogat28@gmail.com>
2026-02-17 15:59:12 +0400

    .

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
                 {/* heading and paragaph start */}
                 <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
                     <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-                        Cushioning for Your Miles
+                        Styllar Test Website 
                     </div>
                     <div className="text-md md:text-xl">
                         A lightweight Nike ZoomX midsole is combined with
